6f5b5035f363558f2f58850b328c9dc0e4d8cde2
aldairdiaz <irving.s21diaz@gmail.com>
2023-09-11 22:43:44 -0600

    Franchise class+

--- franchise.py
+++ franchise.py
@@ -1,3 +1,14 @@
 class Franchise:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, address: str, menus: list) -> None:
+        self.address = address
+        self.menus = menus
+    
+    def __repr__(self) -> str:
+        return self.address
+    
+    def available_menus(self, time: int) -> list:
+        available_menu = []
+        for menu in self.menus:
+            if menu.start_time <= time < menu.end_time:
+                available_menu.append(menu) 
+        return available_menu
--- main.py
+++ main.py
@@ -2,13 +2,20 @@ from menu import Menu
 from franchise import Franchise
 from data import data
 
-
 # Testing
 brunch = Menu('Brunch', data['items_brunch'], 11, 16)
 early_brid = Menu('Early Bird', data['items_early_bird'], 15, 18)
 dinner = Menu('Dinner', data['items_dinner'], 17, 22)
-kinds = Menu('Kids', data['items_kids'], 11, 21)
+kids = Menu('Kids', data['items_kids'], 11, 21)
 
-print(brunch)
-print(brunch.calculate_bill(['pancakes', 'home fries', 'coffee']))
-print(early_brid.calculate_bill(['salumeria plate', 'mushroom ravioli (vegan)']))
+# print(brunch)
+# print(brunch.calculate_bill(['pancakes', 'home fries', 'coffee']))
+# print(early_brid.calculate_bill(['salumeria plate', 'mushroom ravioli (vegan)']))
+
+# Franchise
+flagship_store = Franchise("1232 West End Road",[brunch, early_brid, dinner, kids])
+new_installment = Franchise("12 East Mulberry Street", [brunch, early_brid, dinner, kids])
+
+print(flagship_store.available_menus(17))
+
+# TODO: Creating Businesses!
